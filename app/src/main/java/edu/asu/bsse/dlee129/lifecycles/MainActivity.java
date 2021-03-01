31463bd87c7fad14e618eba87ebe745b159c7b32
David Lee <jolee211@icloud.com>
2021-03-01 12:24:56 -0700

    first commit

---
+++ app/src/main/java/edu/asu/bsse/dlee129/lifecycles/MainActivity.java
@@ -0,0 +1,41 @@
+package edu.asu.bsse.dlee129.lifecycles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+// Copyright 2021 David Lee
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author David Lee    mailto:dlee129@asu.edu
+ * @version February 2021
+ */
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.text);
+    }
+
+    public void alert(View view) {
+        Intent alert = new Intent(this, AlertActivity.class);
+        startActivity(alert);
+    }
+}
